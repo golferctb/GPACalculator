1a003b349765addc0ef689960f928e2c58d201bb
golferctb <golferctb@gmail.com>
2025-07-22 17:43:55 -0500

    Added totalSecs function, need to simplify data for calculation

--- src/Time.cpp
+++ src/Time.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+const int SECONDS_PER_MINUTE = 60;
+const int MINUTES_PER_HOUR = 60;
+const int HOURS_PER_DAY = 24;
+
 class Time {
     private:
         double day;
@@ -7,6 +11,19 @@ class Time {
         double minute;
         double second;
     public:
+        Time() {
+            day = 0;
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
+        Time(double d, double h, double m, double s) {
+            
+            day = d;
+            hour = h;
+            minute = m;
+            second = s;
+        }
         void setDay(double d) {
             day = d;
         }
@@ -30,6 +47,14 @@ class Time {
         }
         double getSecond() {
             return second;
+        }
+        double getTotalSecs() {
+            double totalSecs = (second + (minute * SECONDS_PER_MINUTE) + (hour * MINUTES_PER_HOUR * SECONDS_PER_MINUTE) + (day * HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE));
+            return totalSecs;
+        }
+        double getTotalMinutes() {
+            double totalMinutes = (second / SECONDS_PER_MINUTE) + minute + (hour * MINUTES_PER_HOUR) + (day * HOURS_PER_DAY * MINUTES_PER_HOUR);
+            return totalMinutes;
         }
         
 };
